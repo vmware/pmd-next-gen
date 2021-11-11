3bbecc6847a0e8e09100e469a0fe9bfeed69067a
Susant Sahani <ssahani@vmware.com>
2021-11-11 15:45:04 +0530

    rename system->management

---
+++ plugins/management/management_router.go
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package management
+
+import (
+	"github.com/gorilla/mux"
+
+	"github.com/pm-web/plugins/management/group"
+	"github.com/pm-web/plugins/management/user"
+)
+
+func RegisterRouterManagement(router *mux.Router) {
+	n := router.PathPrefix("/system").Subrouter()
+
+	group.RegisterRouterGroup(n)
+	user.RegisterRouterUser(n)
+}
