05cac937275de4348d223f2581a8000d7f1366bd
Susant Sahani <ssahani@vmware.com>
2021-12-20 17:10:27 +0530

    capability: shorten the code

--- pkg/system/capability.go
+++ pkg/system/capability.go
@@ -4,46 +4,11 @@
 package system
 
 import (
-	"fmt"
-	"strings"
 	"syscall"
 
 	"github.com/syndtr/gocapability/capability"
 	"golang.org/x/sys/unix"
 )
-
-func AllowedCapabilities() ([]capability.Cap, error) {
-	return capSlice([]string{
-		"CAP_CHOWN",
-		"CAP_SYS_ADMIN",
-		"CAP_NET_ADMIN",
-		"CAP_NET_BIND_SERVICE",
-	})
-}
-
-var capabilityMap map[string]capability.Cap
-
-func init() {
-	capabilityMap = make(map[string]capability.Cap, capability.CAP_LAST_CAP+1)
-	for _, c := range capability.List() {
-		if c > capability.CAP_LAST_CAP {
-			continue
-		}
-		capabilityMap["CAP_"+strings.ToUpper(c.String())] = c
-	}
-}
-
-func capSlice(caps []string) ([]capability.Cap, error) {
-	out := make([]capability.Cap, len(caps))
-	for i, c := range caps {
-		v, ok := capabilityMap[c]
-		if !ok {
-			return nil, fmt.Errorf("unknown capability %q", c)
-		}
-		out[i] = v
-	}
-	return out, nil
-}
 
 func ApplyCapability(cred *syscall.Credential) error {
 	caps, err := capability.NewPid2(0)
@@ -52,16 +17,12 @@ func ApplyCapability(cred *syscall.Credential) error {
 	}
 
 	allCapabilityTypes := capability.CAPS | capability.BOUNDS | capability.AMBS
-	allowedCap, err := AllowedCapabilities()
-	if err != nil {
-		return err
-	}
 
 	caps.Clear(capability.CAPS | capability.BOUNDS | capability.AMBS)
-	caps.Set(capability.BOUNDS, allowedCap...)
-	caps.Set(capability.PERMITTED, allowedCap...)
-	caps.Set(capability.INHERITABLE, allowedCap...)
-	caps.Set(capability.EFFECTIVE, allowedCap...)
+	caps.Set(capability.BOUNDS, capability.CAP_NET_ADMIN, capability.CAP_SYS_ADMIN, capability.CAP_NET_BIND_SERVICE)
+	caps.Set(capability.PERMITTED, capability.CAP_NET_ADMIN, capability.CAP_SYS_ADMIN, capability.CAP_NET_BIND_SERVICE)
+	caps.Set(capability.INHERITABLE, capability.CAP_NET_ADMIN, capability.CAP_SYS_ADMIN, capability.CAP_NET_BIND_SERVICE)
+	caps.Set(capability.EFFECTIVE, capability.CAP_NET_ADMIN, capability.CAP_SYS_ADMIN, capability.CAP_NET_BIND_SERVICE)
 
 	caps.Clear(capability.AMBIENT)
 
