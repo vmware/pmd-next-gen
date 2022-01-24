656a0869c33790ec978c6a2a9924b9e645ce89a6
Susant Sahani <ssahani@vmware.com>
2022-01-24 15:16:40 +0530

    validator: introduce IsLinkLocalAddressing

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -12,14 +12,14 @@ import (
 
 func IsBool(str string) bool {
 	switch str {
-	case "1", "t", "T", "true", "TRUE", "True", "YES", "yes", "Yes", "y", "ON", "on", "On":
+	case "1", "t", "T", "true", "TRUE", "True", "YES", "yes", "Yes", "y", "ON", "on", "On", "0", "f", "F",
+		"false", "FALSE", "False", "NO", "no", "No", "n", "OFF", "off", "Off":
 		return true
-	case "0", "f", "F", "false", "FALSE", "False", "NO", "no", "No", "n", "OFF", "off", "Off":
-		return false
 	}
 
 	return false
 }
+
 func IsArrayEmpty(str []string) bool {
 	return len(str) == 0
 }
@@ -52,13 +52,28 @@ func IsVLanId(id string) bool {
 	return err == nil
 }
 
-func IsDHCP(dhcp string) bool {
-	switch dhcp {
+func IsScope(id string) bool {
+	scope, err := strconv.ParseUint(id, 10, 32)
+	if err != nil || scope >= 256 {
+		return false
+	}
+
+	return true
+}
+
+func IsBoolWithIp(s string) bool {
+	switch s {
 	case "yes", "no", "ipv4", "ipv6":
 		return true
 	}
-	
+
 	return false
 }
 
+func IsDHCP(s string) bool {
+	return IsBoolWithIp(s)
+}
 
+func IsLinkLocalAddressing(s string) bool {
+	return IsBoolWithIp(s)
+}
