c4fd47eb0ed3480dc2d9e20db5b7b1bad33fb788
Susant Sahani <ssahani@vmware.com>
2022-02-03 23:04:37 +0530

    system: drop unnessery code

--- pkg/system/capability.go
+++ pkg/system/capability.go
@@ -25,22 +25,13 @@ func ApplyCapability(cred *syscall.Credential) error {
 	caps.Set(capability.EFFECTIVE, capability.CAP_NET_ADMIN, capability.CAP_SYS_ADMIN, capability.CAP_NET_BIND_SERVICE)
 
 	caps.Clear(capability.AMBIENT)
-
 	return caps.Apply(allCapabilityTypes)
 }
 
 func EnableKeepCapability() error {
-	if err := unix.Prctl(unix.PR_SET_KEEPCAPS, 1, 0, 0, 0); err != nil {
-		return err
-	}
-
-	return nil
+	return unix.Prctl(unix.PR_SET_KEEPCAPS, 1, 0, 0, 0)
 }
 
 func DisableKeepCapability() error {
-	if err := unix.Prctl(unix.PR_SET_KEEPCAPS, 0, 0, 0, 0); err != nil {
-		return err
-	}
-
-	return nil
+	return unix.Prctl(unix.PR_SET_KEEPCAPS, 0, 0, 0, 0)
 }
