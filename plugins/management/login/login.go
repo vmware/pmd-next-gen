af13efc815d623c82d7e8b235370c42b1d3ae62a
Nitesh Kumar <kunitesh@vmware.com>
2022-03-28 01:28:57 -0700

    login: Get user and session info based on Id

--- plugins/management/login/login.go
+++ plugins/management/login/login.go
@@ -5,6 +5,8 @@ package login
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	log "github.com/sirupsen/logrus"
@@ -41,7 +43,25 @@ type Session struct {
 	Path string `json:"Path"`
 }
 
-func AcquireUsersFromLogin(ctx context.Context, w http.ResponseWriter) error {
+func decodeSessionJSONRequest(r *http.Request) (*Session, error) {
+	s := Session{}
+	if err := json.NewDecoder(r.Body).Decode(&s); err != nil {
+		return nil, err
+	}
+
+	return &s, nil
+}
+
+func decodeUserJSONRequest(r *http.Request) (*User, error) {
+	u := User{}
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		return nil, err
+	}
+
+	return &u, nil
+}
+
+func AcquireUserListFromLogin(ctx context.Context, w http.ResponseWriter) error {
 	c, err := NewSDConnection()
 	if err != nil {
 		log.Errorf("Failed to establish connection to the system bus: %s", err)
@@ -57,7 +77,7 @@ func AcquireUsersFromLogin(ctx context.Context, w http.ResponseWriter) error {
 	return web.JSONResponse(users, w)
 }
 
-func AcquireSessionsFromLogin(ctx context.Context, w http.ResponseWriter) error {
+func (u *User) AcquireUserFromLogin(ctx context.Context, w http.ResponseWriter) error {
 	c, err := NewSDConnection()
 	if err != nil {
 		log.Errorf("Failed to establish connection to the system bus: %s", err)
@@ -65,10 +85,53 @@ func AcquireSessionsFromLogin(ctx context.Context, w http.ResponseWriter) error 
 	}
 	defer c.Close()
 
-	users, err := c.DBusAcquireUSessionsFromLogin(ctx)
+	users, err := c.DBusAcquireUsersFromLogin(ctx)
 	if err != nil {
 		return web.JSONResponseError(err, w)
 	}
 
-	return web.JSONResponse(users, w)
+	user := User{}
+	for _, usr := range users {
+		if u.UID == usr.UID {
+			user = usr
+			break
+		} else {
+			log.Errorf("User not exist for Uid: %v", u.UID)
+			return fmt.Errorf("No User '%v'", u.UID)
+		}
+	}
+
+	return web.JSONResponse(user.Path, w)
+}
+
+func AcquireSessionListFromLogin(ctx context.Context, w http.ResponseWriter) error {
+	c, err := NewSDConnection()
+	if err != nil {
+		log.Errorf("Failed to establish connection to the system bus: %s", err)
+		return err
+	}
+	defer c.Close()
+
+	sessions, err := c.DBusAcquireUSessionsFromLogin(ctx)
+	if err != nil {
+		return web.JSONResponseError(err, w)
+	}
+
+	return web.JSONResponse(sessions, w)
+}
+
+func (s *Session) AcquireSessionFromLogin(ctx context.Context, w http.ResponseWriter) error {
+	c, err := NewSDConnection()
+	if err != nil {
+		log.Errorf("Failed to establish connection to the system bus: %s", err)
+		return err
+	}
+	defer c.Close()
+
+	session, err := c.DBusAcquireUSessionFromLogin(ctx, s.ID)
+	if err != nil {
+		return web.JSONResponseError(err, w)
+	}
+
+	return web.JSONResponse(session.Path, w)
 }
